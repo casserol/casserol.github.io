e46633c2ee1fe6897c1afd8ae48c3618a298177c
l1h3r <devin.turner09@gmail.com>
2018-11-19 23:08:16 -0800

    modal 2.0

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,94 @@
 (function() {
-  const body = document.body;
+  function make(className, children = []) {
+    const node = document.createElement('div');
+
+    node.className = className;
+
+    for (let child of children) {
+      node.appendChild(child);
+    }
+
+    return node;
+  }
+
+  function modal({
+    title = '',
+    description = '',
+    date = '',
+    client = '',
+    role = '',
+    url = '',
+  } = {}) {
+    return make('modal', [
+      make('overlay'),
+      make('modal-content', [
+        make('modal-header', [
+          make('title', [document.createTextNode(title)]),
+          make('close'),
+        ]),
+        make('modal-body', [
+          make('description', [document.createTextNode(description)]),
+          make('details', [
+            make('', [document.createTextNode(`Date: ${date}`)]),
+            make('', [document.createTextNode(`Client: ${client}`)]),
+            make('', [document.createTextNode(`Role: ${role}`)]),
+            make('', [document.createTextNode(`URL: ${url}`)]),
+          ]),
+        ]),
+      ]),
+    ]);
+  }
+
+  const state = {
+    body: document.body,
+    isOpen: false,
+    modal: null,
+  };
 
   function stop(event) {
     event.preventDefault();
     event.stopPropagation();
   }
 
-  function toggle(modal) {
-    const isOpen = modal.classList.contains('open');
+  function clear() {
+    if (!state.modal) {
+      return;
+    }
 
-    modal.classList.toggle('open', !isOpen);
-    body.classList.toggle('modal-open', !isOpen);
+    document.body.classList.remove('modal-open');
+
+    state.modal.parentNode.removeChild(state.modal);
+    state.modal = null;
+  }
+
+  function toggle(data) {
+    clear();
+
+    state.modal = modal(data);
+
+    document.body.appendChild(state.modal);
+    document.body.classList.add('modal-open');
   }
 
   function click(event) {
     stop(event);
 
-    const card = event.target.closest('.card');
-    const modal = card.querySelector('.modal');
+    const isOverlay = event.target.classList.contains('overlay');
 
-    toggle(modal);
+    if (isOverlay) {
+      clear();
+      return;
+    }
+
+    const card = event.target.closest('.card');
+
+    if (card && card.dataset) {
+      toggle(card.dataset);
+    }
   }
 
   function init() {
-    const nodes = document.querySelectorAll('.card');
-    const cards = Array.from(nodes);
-
-    for (let card of cards) {
-      card.addEventListener('click', click);
-    }
+    document.addEventListener('click', click);
   }
 
   document.addEventListener('DOMContentLoaded', init);
